dece50126781c4c33b6f2f66cefa924265a78c71
dzwdz <kg67199@gmail.com>
2021-01-08 14:20:53 +0100

    add a default bind

--- src/main/java/dzwdz/abacus/Abacus.java
+++ src/main/java/dzwdz/abacus/Abacus.java
@@ -7,6 +7,7 @@ import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -15,7 +16,7 @@ public class Abacus implements ModInitializer {
     public static Renderer renderer;
     public static List<ItemStack> selectedItems = new LinkedList<>();
 
-    public static KeyBinding watchKeybinding = new WatchKeybinding(id("watch"), InputUtil.Type.KEYSYM, -1, "key.categories.abacus", new KeyModifiers());
+    public static KeyBinding watchKeybinding = new WatchKeybinding(id("watch"), InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Z , "key.categories.abacus", new KeyModifiers());
     public static KeyBinding clearKeybinding = new ClearKeybinding(id("clear"), InputUtil.Type.KEYSYM, -1, "key.categories.abacus", new KeyModifiers());
 
     private static Identifier id(String s) {
